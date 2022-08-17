ee60423246a5035dfa1e0753c3fb67033d3cf0a5
Madi Assil <madiasunik@gmail.com>
2022-08-17 17:30:47 +0600

    Module 3 Task #6 - first commit

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        int numberOne = (int) (numberToBeRounded + 0.5);
+        System.out.println(numberOne);
     }
 }
